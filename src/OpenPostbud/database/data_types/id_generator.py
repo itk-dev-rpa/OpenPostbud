a68dd977d53b9a082cb3375831686c976ff34024
Mathias G <ghbm@aarhus.dk>
2025-04-01 10:24:42 +0200

    Added doc strings

--- src/OpenPostbud/database/data_types/id_generator.py
+++ src/OpenPostbud/database/data_types/id_generator.py
@@ -5,8 +5,18 @@ from collections.abc import Callable
 
 
 def create_id(prefix: str, length: int) -> Callable[[], str]:
+    """Return a function that creates random ids with the given prefix and length.
+    The total length is the prefix length + length.
+
+    Args:
+        prefix: The prefix for the created ids.
+        length: The number of random characters to add.
+
+    Returns:
+        A function that creates random ids when called.
+    """    """"""
     def func():
-        CHARACTERS = "abcdefghijklmnopqrstuvwxyz0123456789"
-        return prefix + "".join(secrets.choice(CHARACTERS) for _ in range(length))
+        characters = "abcdefghijklmnopqrstuvwxyz0123456789"
+        return prefix + "".join(secrets.choice(characters) for _ in range(length))
 
     return func
