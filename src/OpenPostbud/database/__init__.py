3835440b830b3513be937dd7d2443ec4a6544996
Mathias G <ghbm@aarhus.dk>
2026-01-16 13:16:18 +0100

    Fixed create_database cli

---
+++ src/OpenPostbud/database/__init__.py
@@ -0,0 +1,4 @@
+from OpenPostbud.database import api_users, audit_log
+from OpenPostbud.database.check_registration import registration_job, registration_task
+from OpenPostbud.database.digital_post import letters, shipments, templates
+from OpenPostbud.database.nemsms import nemsms_messages, nemsms_shipments
