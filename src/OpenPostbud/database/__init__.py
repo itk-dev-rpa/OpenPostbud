c56313ef70af0982da911f4cc33033f7ce456b07
Mathias G <ghbm@aarhus.dk>
2026-02-27 10:56:27 +0100

    Added auto import to database package

--- src/OpenPostbud/database/__init__.py
+++ src/OpenPostbud/database/__init__.py
@@ -10,7 +10,6 @@ def import_submodules(package_name, package_path):
     """Import all modules in the given package."""
     for _, module_name, is_pkg in pkgutil.iter_modules(package_path):
         full_name = f"{package_name}.{module_name}"
-        print(full_name)
         module = importlib.import_module(full_name)
 
         if is_pkg:
